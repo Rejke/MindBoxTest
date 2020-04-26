e8d374e3a0bd3f86fb5aceb4bd5fdf46363e0135
Rejke <Rejke@yandex.ru>
2020-04-26 22:05:06 +0300

    Added Circumference class

---
+++ AreaLib/Circumference.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AreaLib
+{
+    /// <summary>
+    /// Class, that provides functionality for circumference area calculation
+    /// </summary>
+    public class Circumference : Shape
+    {
+        /*
+         *    There is two ways to calculate area.
+         *    First way: like below.
+         *    Second way: override CalculateArea method (ex. CalculateArea() => Math.PI * Dimensions[0] * Dimensions[0];)
+         *    Also, when we choose the second way, must do this: Dimensions = new List<double>{ radius }.AsReadOnly();
+         */
+        
+        /// <param name="radius"></param>
+        public Circumference(double radius)
+        {
+            Dimensions = new List<double>{ radius, radius, Math.PI }.AsReadOnly();
+        }
+    }
+}
