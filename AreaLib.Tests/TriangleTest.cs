815a5713a786de848bfeecb97cc6dbe844b84e5b
Rejke <Rejke@yandex.ru>
2020-04-27 11:28:47 +0300

    Added tests for Triangle

---
+++ AreaLib.Tests/TriangleTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AreaLib.Tests
+{
+    public class TriangleTest
+    {
+        [Fact]
+        public void ZeroTriangleSideTest()
+        {
+            Assert.Throws<ArgumentException>(() => new Triangle(0.0, 2, 3));
+        }
+
+        [Fact]
+        public void NonExistentTriangleTest()
+        {
+            Assert.Throws<ArgumentException>(() => new Triangle(1, 2, 3));
+        }
+
+        [Fact]
+        public void RightTriangleTest()
+        {
+            Assert.True(new Triangle(3, 5, 4).IsRight);
+        }
+
+        [Fact]
+        public void CalculationTest1()
+        {
+            Triangle triangle = new Triangle(3, 4, 5);
+            double area = 0.5 * 3 * 4;
+            Assert.True(triangle.Area == area);
+        }
+
+        [Fact]
+        public void CalculationTest2()
+        {
+            Triangle triangle = new Triangle(5, 5, 5);
+
+            double semiPerimeter = (5.0 + 5.0 + 5.0) / 2.0;
+            double area = Math.Sqrt(semiPerimeter * (semiPerimeter - 5.0) * (semiPerimeter - 5.0) * (semiPerimeter - 5.0));
+            
+            Assert.True(area == triangle.Area);
+        }
+    }
+}
